726d6b4a9cac8d7ed0642cbbc04a1ad0259e2951
Lien <llagasneilandrew@gmail.com>
2025-04-24 08:45:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "https://whimsical-parfait-dac91a.netlify.app/",
+    "http://localhost",
+    "http://localhost:8081", 
+    "http://127.0.0.1",
+    "http://127.0.0.1:8081",
 ]
 
 app.add_middleware(
